0400f9abbcc9b67bcc7c0fde495a75ddabb6a8be
Khachik Khachatryan <kkhachat@c2r9s1.42yerevan.am>
2026-02-11 20:14:29 +0400

    3

---
+++ complex_algorithm.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   complex_algorithm.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkhachat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 16:11:38 by kkhachat          #+#    #+#             */
+/*   Updated: 2026/02/11 20:05:58 by kkhachat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	transfer_left_run_to_b(t_context *c, int lenL)
+{
+	int	i;
+
+	i = 0;
+	while (i < lenL)
+	{
+		op_pb(c);
+		op_rb(c);
+		i++;
+	}
+}
+
+static void	merge_round(t_context *c, int len)
+{
+	int	n;
+	int	processed;
+	int	len_l;
+	int	len_r;
+	int	j;
+
+	n = c->a.size;
+	processed = 0;
+	while (processed < n)
+	{
+		len_l = min_int(len, n - processed);
+		len_r = min_int(len, n - processed - len_l);
+		if (len_r > 0)
+			merge_runs(c, len_l, len_r);
+		else
+		{
+			j = 0;
+			while (j++ < len_l)
+				op_ra(c);
+		}
+		processed += (len_l + len_r);
+	}
+}
+
+static void	rotate_to_min_helper(t_context *c, int *min_val,
+			int *min_idx, int i)
+{
+	if (c->a.data[i] < *min_val)
+	{
+		*min_val = c->a.data[i];
+		*min_idx = i;
+	}
+}
+
+static void	rotate_to_min(t_context *c)
+{
+	int	min_val;
+	int	min_idx;
+	int	i;
+	int	k;
+
+	min_val = c->a.data[0];
+	min_idx = 0;
+	i = 1;
+	while (i < c->a.size)
+	{
+		rotate_to_min_helper(c, &min_val, &min_idx, i);
+		i++;
+	}
+	if (min_idx <= c->a.size / 2)
+	{
+		k = min_idx;
+		while (k--)
+			op_ra(c);
+	}
+	else
+	{
+		k = c->a.size - min_idx;
+		while (k--)
+			op_rra(c);
+	}
+}
+
+void	merge_sort(t_context *c)
+{
+	int	len;
+
+	len = 1;
+	while (len < c->a.size)
+	{
+		merge_round(c, len);
+		len *= 2;
+	}
+	rotate_to_min(c);
+}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kkhachat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 20:10:42 by kkhachat          #+#    #+#             */
-/*   Updated: 2026/02/10 20:34:10 by kkhachat         ###   ########.fr       */
+/*   Updated: 2026/02/11 20:14:13 by kkhachat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,8 @@ void	op_rrb(t_context *c);
 void	op_rrr(t_context *c);
 
 void	bubble_sort(t_context *c);
+int		min_int(int a, int b);
+void	merge_runs(t_context *c, int lenL, int lenR);
+void	merge_sort(t_context *c);
 
 #endif
